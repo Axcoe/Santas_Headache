58bec071aab62f0c764beca830b62a75e7c5bd1b
Alice Duvillier <alice.duvillier@epitech.eu>
2026-02-20 11:04:14 +0100

    add: archi for object

---
+++ src/object/wrap/Wrap.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2026
+** santa_rush4_2
+** File description:
+** Wrap
+*/
+
+#include "Wrap.hpp"
+
+Wrap::Wrap()
+{
+}
+
+Wrap::~Wrap()
+{
+}
---
+++ src/object/wrap/Wrap.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2026
+** santa_rush4_2
+** File description:
+** Wrap
+*/
+
+#ifndef WRAP_HPP_
+#define WRAP_HPP_
+
+class Wrap {
+    public:
+        Wrap();
+        ~Wrap();
+
+    protected:
+    private:
+};
+
+#endif /* !WRAP_HPP_ */
