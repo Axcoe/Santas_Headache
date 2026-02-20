8b297ae62670de2b43a12b0f445e8d1b93a25873
Alice Duvillier <alice.duvillier@epitech.eu>
2026-02-20 11:08:44 +0100

    add: objct class

---
+++ src/object/Object.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2026
+** santa_rush4_2
+** File description:
+** Object
+*/
+
+#include "Object.hpp"
+
+Object::Object()
+{
+}
+
+Object::~Object()
+{
+}
---
+++ src/object/Object.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2026
+** santa_rush4_2
+** File description:
+** Object
+*/
+
+#ifndef OBJECT_HPP_
+#define OBJECT_HPP_
+
+class Object {
+    public:
+        Object();
+        ~Object();
+
+    protected:
+    private:
+};
+
+#endif /* !OBJECT_HPP_ */
