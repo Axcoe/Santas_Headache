3523a4879dc2f76fc87409422e335334506f05ce
Alice Duvillier <alice.duvillier@epitech.eu>
2026-02-20 16:13:45 +0100

    add: conveyor belt and functions

---
+++ src/conveyorbelt/PapaXMasConveyorBelt.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2026
+** santa_rush4_2
+** File description:
+** PapaXMasConvoyerBelt
+*/
+
+#ifndef PAPAXMASCONVOYERBELT_HPP_
+#define PAPAXMASCONVOYERBELT_HPP_
+
+#include "IConveyorBelt.hpp"
+
+class PapaXMasConveyorBelt : public IConveyorBelt {
+    public:
+        PapaXMasConveyorBelt();
+        ~PapaXMasConveyorBelt() = default;
+
+        void putOnConveyor(Object *obj) override;
+        Object *takeFromConveyor() override;
+        void in() override;
+        void out() override;
+
+    protected:
+        Object *_obj = nullptr;
+        bool _isEmpty = true;
+
+    private:
+};
+
+#endif /* !PAPAXMASCONVOYERBELT_HPP_ */
