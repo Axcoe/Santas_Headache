71a2c4a30cff094ba386991cd98e7b124c4491ec
marion <marion.mousset@epitech.eu>
2026-02-20 16:35:47 +0100

    add: extend IElf interface and PapaXMasElf class to support conveyor belt operations

--- src/Elf/PapaXMasElf.cpp
+++ src/Elf/PapaXMasElf.cpp
@@ -29,12 +29,28 @@ void PapaXMasElf::take(ITable *table, int i)
     }
 }
 
+void PapaXMasElf::take(IConveyorBelt *cBelt, int i)
+{
+    cBelt->takeFromConveyor();
+}
+
 void PapaXMasElf::put(ITable *table, Object *obj)
 {
     table->putOnTable(obj);
 }
 
+void PapaXMasElf::put(IConveyorBelt *cBelt, Object *obj)
+{
+    cBelt->putOnConveyor(obj);
+}
+
 Object **PapaXMasElf::look(ITable *table) const
 {
     return table->getObjects();
+}
+
+void PapaXMasElf::out(Object *obj, IConveyorBelt *cBelt)
+{
+    cBelt->putOnConveyor(obj);
+    cBelt->out();
 }
