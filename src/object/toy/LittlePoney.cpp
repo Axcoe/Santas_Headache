782a725b03d747cc0270642046c86785709b12cb
marion <marion.mousset@epitech.eu>
2026-02-20 11:48:17 +0100

    add: implement LittlePoney and Teddy toy classes with basic functionality

---
+++ src/object/toy/LittlePoney.cpp
@@ -0,0 +1,38 @@
+/* ------------------------------------------------------------------------------------ *
+ *                                                                                      *
+ * EPITECH PROJECT - Sat, Feb, 2026                                                     *
+ * Title           - Santas_Headache                                                    *
+ * Description     -                                                                    *
+ *     LittlePoney                                                                      *
+ *                                                                                      *
+ * ------------------------------------------------------------------------------------ *
+ *                                                                                      *
+ *         ░        ░       ░░        ░        ░        ░░      ░░  ░░░░  ░             *
+ *         ▒  ▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒▒▒  ▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒▒▒▒▒▒  ▒▒▒▒  ▒  ▒▒▒▒  ▒             *
+ *         ▓      ▓▓▓       ▓▓▓▓▓  ▓▓▓▓▓▓▓  ▓▓▓▓      ▓▓▓  ▓▓▓▓▓▓▓        ▓             *
+ *         █  ███████  ██████████  ███████  ████  ███████  ████  █  ████  █             *
+ *         █        █  ███████        ████  ████        ██      ██  ████  █             *
+ *                                                                                      *
+ * ------------------------------------------------------------------------------------ */
+
+#include "LittlePoney.hpp"
+
+LittlePoney::LittlePoney()
+{
+    // Default constructor
+}
+
+void LittlePoney::isTaken()
+{
+    _taken = true;
+    std::cout << "yo man" << std::endl;
+}
+
+std::ostream& operator<<(std::ostream &stream, const LittlePoney& littlePoney)
+{
+    if (littlePoney.getTaken())
+        stream << "LittlePoney is taken";
+    else
+        stream << "LittlePoney isn't taken";
+    return stream;
+}
---
+++ src/object/toy/Teddy.cpp
@@ -0,0 +1,38 @@
+/* ------------------------------------------------------------------------------------ *
+ *                                                                                      *
+ * EPITECH PROJECT - Sat, Feb, 2026                                                     *
+ * Title           - Santas_Headache                                                    *
+ * Description     -                                                                    *
+ *     Teddy                                                                            *
+ *                                                                                      *
+ * ------------------------------------------------------------------------------------ *
+ *                                                                                      *
+ *         ░        ░       ░░        ░        ░        ░░      ░░  ░░░░  ░             *
+ *         ▒  ▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒▒▒  ▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒▒▒▒▒▒  ▒▒▒▒  ▒  ▒▒▒▒  ▒             *
+ *         ▓      ▓▓▓       ▓▓▓▓▓  ▓▓▓▓▓▓▓  ▓▓▓▓      ▓▓▓  ▓▓▓▓▓▓▓        ▓             *
+ *         █  ███████  ██████████  ███████  ████  ███████  ████  █  ████  █             *
+ *         █        █  ███████        ████  ████        ██      ██  ████  █             *
+ *                                                                                      *
+ * ------------------------------------------------------------------------------------ */
+
+#include "Teddy.hpp"
+
+Teddy::Teddy()
+{
+    // Default constructor
+}
+
+void Teddy::isTaken()
+{
+    _taken = true;
+    std::cout << "gra hu" << std::endl;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Teddy& teddy)
+{
+    if (teddy.getTaken())
+        stream << "Teddy is taken";
+    else
+        stream << "Teddy isn't taken";
+    return stream;
+}
